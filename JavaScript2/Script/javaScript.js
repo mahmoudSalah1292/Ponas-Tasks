c42edaba9f2d6edfee4a324aa4e86e607a4ceada
Mahmoud.Salah1292 <134862577+mahmoudSalah1292@users.noreply.github.com>
2023-06-25 05:55:28 +0300

    Add files via upload

---
+++ JavaScript2/Script/javaScript.js
@@ -0,0 +1,13 @@
+
+// let ul = document.getElementById('ch-color')
+
+// function changeClick(flag) {
+//     (flag == 'onclick') ? li.classList.add(`chng-co`) : i.classList.remove(`chng-co`)
+// }
+
+
+let i = 0
+
+let set setInterval(() => {
+    if (i ==200) clearInterval
+}, interval);
